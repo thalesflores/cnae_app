26cf6600cbf4c138f78e87e85a8be5116a3d17bb
Thales Flores <thalesflores@Thaless-MacBook-Pro.local>
2018-02-28 06:45:33 -0300

    dotenv configs

--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,12 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Dotenv::Railtie.load
 module CnabParser
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.autoload_paths += Dir["#{Rails.root}/app/api/*"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
