ef554d361ae863671699414ffda06df021afaf0a
Thales Flores <thalesflores@Thaless-MacBook-Pro.local>
2018-03-01 00:26:40 -0300

    Add save function to the controller

--- app/controllers/api/v1/cnaes_controller.rb
+++ app/controllers/api/v1/cnaes_controller.rb
@@ -3,21 +3,24 @@ module Api
     class CnaesController < ::ApplicationController
 
       def index
+        response = Cnae.all
+        render json: response.to_json
       end
 
       def save
-        transactions = ::Integrations::Cnab.new.extract
-        find_correct(transactions)
-        render json: integration.to_json
+        response = params_save
+        response.each { |transaction| Cnae.create_new(transaction) }
+        render json: { message: 'Saved' }, status: 200
       end
 
       private
 
-       def find_correct(transactions)
-         transactions.map! do |transaction|
-          
-         end
-       end
+      def params_save
+        transactions = ::Integrations::Cnab.new.extract
+        validated = ::Validations::Transactions.find_correct(transactions)
+        ::Constructor::Transactions.new(validated).generate
+      end
+
     end
   end
 end
