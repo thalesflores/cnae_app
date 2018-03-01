b3344322b1f88d877b105dc40e690659d87ba7ad
Thales Flores <thalesflores@Thaless-MacBook-Pro.local>
2018-03-01 00:27:39 -0300

    Add a scope to save only one transaction and relationship with type model/table

--- app/models/cnae.rb
+++ app/models/cnae.rb
@@ -1,2 +1,13 @@
 class Cnae < ApplicationRecord
+  has_one :type
+
+  # transaction type enum
+  enum type: { debit: '1', credit: '4' }
+
+  # Scope
+  scope :create_new,->(transaction){
+        where(cpf: transaction[:cpf])
+          .where(date: transaction[:date])
+          .where(value: transaction[:value])
+          .first_or_create! } # validate if the transaction has already been saved
 end
