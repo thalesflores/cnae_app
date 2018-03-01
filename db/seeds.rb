4815bbfdc349d58b18df65439b50003cf26eaf3e
Thales Flores <thalesflores@Thaless-MacBook-Pro.local>
2018-03-01 00:28:46 -0300

    seed to add types in table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+types = %w[debit tax financing credit loan]
+types.each { |type| Type.create({description: type}) }
