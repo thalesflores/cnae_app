bb95c1a7211496ec71dafbebeab314470fefa231
Thales <thales.flores@icommgroup.com.br>
2018-03-01 17:34:02 -0300

    Index with filters

--- app/controllers/api/v1/cnaes_controller.rb
+++ app/controllers/api/v1/cnaes_controller.rb
@@ -1,15 +1,17 @@
 module Api
   module V1
     class CnaesController < ::ApplicationController
+      before_action :auth_token
 
       def index
-        response = Cnae.all
+        @transactions = Cnae.where(nil)
+        response = params_filter
         render json: response.to_json
       end
 
       def save
         response = params_save
-        response.each { |transaction| Cnae.create_new(transaction) }
+        response.each { |transaction| Cnae.create(transaction) }
         render json: { message: 'Saved' }, status: 200
       end
 
@@ -21,6 +23,16 @@ module Api
         ::Constructor::Transactions.new(validated).generate
       end
 
+      def params_filter
+        filtering.each do |key, value|
+          @transactions = @transactions.public_send(key, value) if value.present?
+        end
+        @transactions 
+      end
+
+      def filtering
+        params.slice(:type, :value, :cpf, :card)
+      end
     end
   end
 end
